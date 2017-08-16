293db7d6e243a627fe03ba7a93999ee3cff45e23
nihilates <nihilates@gmail.com>
2017-08-15 21:42:20 -0400

    Setting items API behind authentication requirement

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -3,13 +3,11 @@ const hlp = require('../helper');
 
 //API routes for the Items table
 module.exports = (app, db) => {
-  app.get('/api/items', (req, res) => {
-    console.log('Attempting Query...')
-    db.Items.findAll().then((items) => {
-      console.log('Successful Query')
+
+  app.get('/api/auth/items', (req, res) => {
+    db.Items.findAll().then(items => {
       hlp.respQuery(items, req, res);
     }).catch((err) => {
-      console.log('Failed Query')
       hlp.respErr(err, req, res);
     });
   });
