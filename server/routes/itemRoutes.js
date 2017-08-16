3c57cdbe0cdbc178a1ad0b3e4594c104462af77e
nihilates <nihilates@gmail.com>
2017-08-16 11:56:35 -0400

    Adding Type db API

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -4,11 +4,24 @@ const hlp = require('../helper');
 //API routes for the Items table
 module.exports = (app, db) => {
 
+  //gets a list of all items on the items table; this will change to be more specific when we go live
   app.get('/api/auth/items', (req, res) => {
-    db.Items.findAll().then((items) => {
+    db.Items.findAll().then(items => {
       hlp.respQuery(items, req, res);
     }).catch((err) => {
       hlp.respErr(err, req, res);
     });
   });
+
+  //gets a specific item type
+  app.get('/api/items/type', (req, res) => {
+    let typeId = req.query.typeId;
+
+    db.Items.find({where: {id: typeId}}).then(type => {
+      hlp.respQuery(type, req, res);
+    }).catch((err) => {
+      hlp.respErr(err, req, res);
+    });
+  });
+
 };
