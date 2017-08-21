0eb07b8a6291f717759fba36c107f90e5f16df70
nihilates <nihilates@gmail.com>
2017-08-21 17:00:01 -0400

    Expanding Inventory APIs

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -8,7 +8,16 @@ module.exports = (app, db) => {
     let charId = req.query.charId;
 
     db.Inventory.findAll({where: {charId: charId}}).then(entries => {
-      hlp.respQuery(entries, req, res);
+      //Process the inventory entries here
+      let result = entries;
+
+      result.forEach(entry => {
+        db.Items.find({where: {id: entry.itemId}}).then(itemName => {
+          entry.itemName = itemName;
+        })
+      });
+
+      hlp.respQuery(result, req, res);
     }).catch((err) => {
       hlp.respErr(err, req, res);
     });
