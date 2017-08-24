40ca22f0781596c5c3784cee56f3b5e05420d06c
nihilates <nihilates@gmail.com>
2017-08-23 21:28:52 -0400

    Building out Delete API

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -45,26 +45,14 @@ module.exports = (app, db) => {
         }, {where: {id: req.body.id}});
         hlp.respQuery(inventory, req, res);
       }
-    }).catch(err => {
+    }).catch((err) => {
       hlp.respErr(err, req, res);
     });
   });
 
   app.delete('/api/inventory/remove', (req, res) => {
-    // let charId = req.body.charId;
-    // let id = req.body.id;
-
-    console.log(req.body)
-
-    console.log('CharId:', charId)
-    console.log('id:', id)
-
-    // db.Inventory.destroy({where: {$and: [{id: id}, {charId: charId}]}})
-    // .then(death => {
-    //   hlp.respQuery(death, req, res)
-    // }).catch(err => {
-    //   hlp.respErr(err, req, res);
-    // })
-  });
+    console.log(req)
+    res.status(200).send('Recieved');
+  })
 
 };
