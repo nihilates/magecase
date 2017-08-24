bf3ba66a983bd67d5804b15ec13ac001268851db
nihilates <nihilates@gmail.com>
2017-08-24 00:23:27 -0400

    Building out Delete API

--- server/helper.js
+++ server/helper.js
@@ -37,7 +37,7 @@ module.exports.respQuery = (dbResp, req, res, reqToken) => { //reqToken is an op
     var userData = _.pick(dbResp.dataValues, ['id', 'user_name', 'user_email', 'user_icon', 'premium_status']);
 //token isn't required? Send just the dbResp.      : Otherwise, send the dbResp and a new token pair
     // !reqToken ? res.status(200).send(dbResp).end() : res.status(200).send({dbResp, auth: {id_token: createIdToken(dbResp.dataValues), access_token: createAccessToken()}});
-    !reqToken ? res.status(200).sendStatus(dbResp).end() : res.status(200).sendStatus({userData, auth: {id_token: createIdToken(userData), access_token: createAccessToken()}});
+    !reqToken ? res.status(200).send(dbResp).end() : res.status(200).send({userData, auth: {id_token: createIdToken(userData), access_token: createAccessToken()}});
   } else {
     res.status(204).send('No matching results');
   }
@@ -45,7 +45,7 @@ module.exports.respQuery = (dbResp, req, res, reqToken) => { //reqToken is an op
 
 module.exports.respErr = (dbResp, req, res) => {
   // res.json({error: dbResp}).status(500).send;
-  res.status(500).sendStatus({error: dbResp}).end();
+  res.status(500).send({error: dbResp}).end();
 };
 
 //function to parse a submitted identifier and determin it it's a username or user email
