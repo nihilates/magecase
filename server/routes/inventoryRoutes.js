0d2b585f6e9b1fa23919679d1cb7b9d63e6e2d5a
nihilates <nihilates@gmail.com>
2017-08-23 20:03:27 -0400

    Building out Delete API

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -51,7 +51,7 @@ module.exports = (app, db) => {
   });
 
   app.delete('/api/inventory/remove', (req, res) => {
-    console.log(req)
+    console.log(req.body)
     res.status(200).send('Recieved');
   })
 
