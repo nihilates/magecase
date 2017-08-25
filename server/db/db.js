667361260869244a9f71dd73b84705edef3bf139
nihilates <nihilates@gmail.com>
2017-08-24 21:11:15 -0400

    Defining better item APIs

--- server/db/db.js
+++ server/db/db.js
@@ -158,8 +158,8 @@ module.exports.syncTables = (force, schema) => {
   module.exports.ItemSubtypes.belongsTo(module.exports.ItemTypes, {as: 'itemType'});
   //Items Table Foreign-Keys
   module.exports.Items.belongsTo(module.exports.ItemTypes, {as: 'type'});
-  module.exports.Items.belongsTo(module.exports.ItemSubtypes, {as: 'subType'});
-  module.exports.Items.belongsTo(module.exports.Users);
+  // module.exports.Items.belongsTo(module.exports.ItemSubtypes, {as: 'subType'});
+  // module.exports.Items.belongsTo(module.exports.Users);
   //Asset Types Table Foreign-Keys
   module.exports.AssetTypes.belongsTo(module.exports.Users);
   //Asset Table Foreign-Keys
