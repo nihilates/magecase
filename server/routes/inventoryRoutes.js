602688f00a8ad9c649ad2d3879526406c2302eba
nihilates <nihilates@gmail.com>
2017-08-26 13:11:22 -0400

    Working out ineventory routes to dynamically pull item types

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app, db) => {
       where: {charId: charId},
       // include: db.Items
       include: [
-        {model: db.Items, include: [db.ItemTypes, db.ItemSubTypes]}
+        {model: db.Items, include: [db.ItemTypes]}
       ]}).then(entries => {
       hlp.respQuery(entries, req, res);
     }).catch((err) => {
