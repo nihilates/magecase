2f0fd0d37f050ec05c8079727587c568c1a45dbc
nihilates <nihilates@gmail.com>
2017-09-01 12:08:28 -0400

    Testing omni-route method

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app, db) => {
       include: [
         db.Characters,
         db.Games,
-        {model: db.CurrencySystems},
+        {model: db.CurrencySystems, include: db.CurrencyUnits},
         db.Items,
         db.AssetTypes,
         db.ShopTypes,
