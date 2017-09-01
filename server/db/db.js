d6b1c6bb201fb30efebf462d4b6e3204b1a2c301
nihilates <nihilates@gmail.com>
2017-09-01 12:50:49 -0400

    Testing omni-route method

--- server/db/db.js
+++ server/db/db.js
@@ -163,7 +163,7 @@ module.exports.syncTables = (force, schema) => {
   module.exports.Games.belongsTo(module.exports.Users);
   module.exports.Games.belongsTo(module.exports.CurrencySystems, {as: 'currency'});
   //Currency Systems Table Foreign-Keys
-  module.exports.CurrencySystems.hasMany(module.exports.CurrencyUnits, {as: 'currency'});
+  module.exports.CurrencySystems.hasMany(module.exports.CurrencyUnits, {as: 'currencyId'});
 
   module.exports.CurrencySystems.belongsTo(module.exports.Users);
   //Currency Units Table Foreign-Keys
