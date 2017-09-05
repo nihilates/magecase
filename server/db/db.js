ac8b9a091ef3dc1940ccc78afc9f9a0b66b59ce7
nihilates <nihilates@gmail.com>
2017-09-05 12:33:33 -0400

    Building out default data APIs to load on application launch: currency

--- server/db/db.js
+++ server/db/db.js
@@ -163,7 +163,7 @@ module.exports.syncTables = (force, schema) => {
   module.exports.Games.belongsTo(module.exports.Users);
   module.exports.Games.belongsTo(module.exports.CurrencySystems, {foreignKey: 'currencySystemId'});
   //Currency Systems Table Foreign-Keys
-  module.exports.CurrencySystems.hasMany(module.exports.CurrencyUnits);
+  module.exports.CurrencySystems.hasMany(module.exports.CurrencyUnits, {as: 'units'});
 
   module.exports.CurrencySystems.belongsTo(module.exports.Users);
   //Currency Units Table Foreign-Keys
