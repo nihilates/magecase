87bda1f2a26d82d98bebaee9630a8a7df1ccf3a0
nihilates <nihilates@gmail.com>
2017-09-06 17:03:45 -0400

    Helper cleanup

--- server/helper.js
+++ server/helper.js
@@ -36,8 +36,8 @@ module.exports.respQuery = (dbResp, req, res, reqToken) => { //reqToken is an op
   if (dbResp) {
     var userData = _.pick(dbResp.dataValues, ['id', 'user_name', 'user_email', 'user_icon', 'premium_status']);
 //token isn't required? Send just the dbResp.      : Otherwise, send the dbResp and a new token pair
-    // !reqToken ? res.status(200).send(dbResp).end() : res.status(200).send({dbResp, auth: {id_token: createIdToken(dbResp.dataValues), access_token: createAccessToken()}});
     !reqToken ? res.status(200).send(dbResp).end() : res.status(200).send({userData, auth: {id_token: createIdToken(userData), access_token: createAccessToken()}});
+    // !reqToken ? res.status(200).send(dbResp).end() : res.status(200).send({id_token: createIdToken(userData), access_token: createAccessToken()});
   } else {
     res.status(204).send('No matching results');
   }
